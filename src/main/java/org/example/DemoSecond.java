14145e48d49ca1d57a51d56c8141c215658f7ec9
vishalOwner <vishalwaykos9@gmail.com>
2023-10-30 16:44:21 +0530

    this is continued commited

---
+++ src/main/java/org/example/DemoSecond.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class DemoSecond {
+        int no;
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+}
